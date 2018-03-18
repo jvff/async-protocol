ddc20ee408c9571252c3cc71527ae596765af006
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-03-18 15:54:51 +0000

    Create RequestSender type
    A Future that only is complete after it has successfully sent an item to
    a Sink.

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,5 @@ mod ready_queue;
 mod fifo_dispatcher;
 mod dispatcher;
 mod receiver;
+
+mod request_sender;
---
+++ src/request_sender.rs
@@ -0,0 +1,48 @@
+use std::sync::{Arc, Mutex};
+
+use futures::{Async, AsyncSink, Future, Poll, Sink};
+
+pub struct RequestSender<T>
+where
+    T: Sink,
+{
+    sink: Arc<Mutex<T>>,
+    request: Option<T::SinkItem>,
+}
+
+impl<T> RequestSender<T>
+where
+    T: Sink,
+{
+    pub fn new(sink: Arc<Mutex<T>>, request: T::SinkItem) -> Self {
+        RequestSender {
+            sink,
+            request: Some(request),
+        }
+    }
+}
+
+impl<T> Future for RequestSender<T>
+where
+    T: Sink,
+{
+    type Item = ();
+    type Error = T::SinkError;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let mut sink = self.sink.lock()
+            .expect("a thread panicked while holding RequestSender locked");
+
+        if let Some(request) = self.request.take() {
+            match sink.start_send(request)? {
+                AsyncSink::Ready => (),
+                AsyncSink::NotReady(request) => {
+                    self.request = Some(request);
+                    return Ok(Async::NotReady);
+                }
+            }
+        }
+
+        sink.poll_complete()
+    }
+}
