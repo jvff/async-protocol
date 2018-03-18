4dd1f9b4e91f0a759958f6723d25fa05d35a2f41
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-03-18 16:03:54 +0000

    Test PipelineClient

--- src/pipeline_client.rs
+++ src/pipeline_client.rs
@@ -57,3 +57,92 @@ where
         receiver.flatten()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use futures::Async;
+    use futures::sync::mpsc;
+
+    use tests::common::SinkStream;
+    use super::*;
+
+    #[test]
+    fn simple_operation() {
+        let (mut in_tx, in_rx) = mpsc::channel(2);
+        let (out_tx, mut out_rx) = mpsc::channel(2);
+        let transport = SinkStream::new(out_tx, in_rx);
+
+        let client = PipelineClient::new(transport);
+        let client_service = &client;
+
+        let first_request = "first request";
+        let second_request = "second request";
+
+        let first_call = client_service.call(first_request.to_string());
+        let second_call = client_service.call(second_request.to_string());
+
+        let first_response = "first response";
+        let second_response = "second response";
+
+        in_tx.try_send(first_response.to_string()).unwrap();
+        in_tx.try_send(second_response.to_string()).unwrap();
+
+        let calls = first_call.join(second_call);
+        let (first_result, second_result) = calls.wait().unwrap();
+
+        assert_eq!(receive(&mut out_rx), first_request);
+        assert_eq!(receive(&mut out_rx), second_request);
+
+        assert_eq!(first_result, first_response);
+        assert_eq!(second_result, second_response);
+    }
+
+    #[test]
+    fn inverted_join() {
+        let (mut in_tx, in_rx) = mpsc::channel(2);
+        let (out_tx, mut out_rx) = mpsc::channel(2);
+        let transport = SinkStream::new(out_tx, in_rx);
+
+        let client = PipelineClient::new(transport);
+        let client_service = &client;
+
+        let first_request = "first request";
+        let second_request = "second request";
+
+        let first_call = client_service.call(first_request.to_string());
+        let second_call = client_service.call(second_request.to_string());
+
+        let first_response = "first response";
+        let second_response = "second response";
+
+        in_tx.try_send(first_response.to_string()).unwrap();
+        in_tx.try_send(second_response.to_string()).unwrap();
+
+        let calls = second_call.join(first_call);
+        let (first_result, second_result) = calls.wait().unwrap();
+
+        assert_eq!(receive(&mut out_rx), second_request);
+        assert_eq!(receive(&mut out_rx), first_request);
+
+        assert_eq!(first_result, first_response);
+        assert_eq!(second_result, second_response);
+    }
+
+    fn receive<S>(stream: &mut S) -> S::Item
+    where
+        S: Stream,
+    {
+        match stream.poll() {
+            Ok(Async::Ready(Some(item))) => item,
+            Ok(Async::Ready(None)) => {
+                panic!("failed to receive item from stream: Stream is empty");
+            }
+            Ok(Async::NotReady) => {
+                panic!("failed to receive item from stream: Not Ready");
+            }
+            Err(_) => {
+                panic!("failed to receive item from stream: Error");
+            }
+        }
+    }
+}
