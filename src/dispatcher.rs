afc776b6496757f3ef643effae49fe1cbb6ea981
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-03-18 15:49:13 +0000

    Add Dispatcher::spawn_receiver method
    Spawns a Future that will receive a result from the Dispatcher. Sets the
    Receiver's ID accordingly.

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -1,9 +1,16 @@
 use futures::Poll;
 
+use super::receiver::Receiver;
+
 pub trait Dispatcher {
     type Item;
     type Error;
     type Id;
+
+    fn spawn_receiver(&self) -> Receiver<Self>
+    where
+        Self: Sized,
+    ;
 
     fn poll(&self, id: &Self::Id) -> Poll<Self::Item, Self::Error>;
 }
