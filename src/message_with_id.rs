426e33a3279d2e3105f0e255df3e04b786d85f5d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-03-28 21:01:17 +0000

    Implement MessageWithId for tuples with integers

--- src/message_with_id.rs
+++ src/message_with_id.rs
@@ -3,3 +3,52 @@ pub trait MessageWithId {
 
     fn id(&self) -> Self::Id;
 }
+
+macro_rules! impl_for_pair_tuples {
+    ($type:ty $(,)*) => {
+        impl<T> MessageWithId for ($type, T) {
+            type Id = $type;
+
+            fn id(&self) -> Self::Id {
+                self.0
+            }
+        }
+    };
+
+    ($type:ty $(, $rest:ty)* $(,)*) => {
+        impl_for_pair_tuples!( $type );
+        impl_for_pair_tuples!( $($rest),* );
+    };
+}
+
+impl_for_pair_tuples! {
+    u8,
+    u16,
+    u32,
+    u64,
+    usize,
+    i8,
+    i16,
+    i32,
+    i64,
+    isize,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pair_tuple_implementation_for_u8() {
+        let message = (10u8, ());
+
+        assert_eq!(message.id(), 10u8);
+    }
+
+    #[test]
+    fn pair_tuple_implementation_for_isize() {
+        let message = (1_000isize, "dummy string");
+
+        assert_eq!(message.id(), 1_000isize);
+    }
+}
