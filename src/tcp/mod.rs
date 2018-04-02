c6f88b0b0469bde0daccd24826eb3b15e561a17d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-04-02 15:28:09 +0000

    Create PipelineTcpClient type

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -1,6 +1,8 @@
 mod incoming_transports;
 mod tcp_client_transport;
 
+mod pipeline_tcp_client;
+
 mod generic_tcp_server;
 mod multiplex_tcp_server;
 mod pipeline_tcp_server;
@@ -8,6 +10,8 @@ mod pipeline_tcp_server;
 mod generic_tcp_listener_server;
 mod multiplex_tcp_listener_server;
 mod pipeline_tcp_listener_server;
+
+pub use self::pipeline_tcp_client::PipelineTcpClient;
 
 pub use self::multiplex_tcp_server::MultiplexTcpServer;
 pub use self::pipeline_tcp_server::PipelineTcpServer;
---
+++ src/tcp/pipeline_tcp_client.rs
@@ -0,0 +1,47 @@
+use std::net::SocketAddr;
+
+use tokio_core::reactor::Handle;
+use tokio_io::codec::{Decoder, Encoder};
+use tokio_service::Service;
+
+use super::{
+    super::{client_error::ClientError, pipeline_client::PipelineClient},
+    tcp_client_transport::TcpClientTransport,
+};
+
+pub struct PipelineTcpClient<C>
+where
+    C: Decoder + Encoder,
+{
+    client: PipelineClient<TcpClientTransport<C>>,
+}
+
+impl<C> PipelineTcpClient<C>
+where
+    C: Decoder + Encoder,
+{
+    pub fn connect(address: &SocketAddr, codec: C, handle: &Handle) -> Self {
+        let transport = TcpClientTransport::connect(address, codec, handle);
+
+        PipelineTcpClient {
+            client: PipelineClient::new(transport),
+        }
+    }
+}
+
+impl<'s, C> Service for &'s PipelineTcpClient<C>
+where
+    C: Decoder + Encoder,
+{
+    type Request = <C as Encoder>::Item;
+    type Response = <C as Decoder>::Item;
+    type Error = ClientError<<C as Decoder>::Error, <C as Encoder>::Error>;
+    type Future =
+        <&'s PipelineClient<TcpClientTransport<C>> as Service>::Future;
+
+    fn call(&self, request: Self::Request) -> Self::Future {
+        let client = &self.client;
+
+        client.call(request)
+    }
+}
