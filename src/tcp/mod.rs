f10d2c59b132aec36da81f5f67bd5bd10e77cdd7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-04-02 15:34:25 +0000

    Create MultiplexTcpClient type

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -1,6 +1,7 @@
 mod incoming_transports;
 mod tcp_client_transport;
 
+mod multiplex_tcp_client;
 mod pipeline_tcp_client;
 
 mod generic_tcp_server;
@@ -11,6 +12,7 @@ mod generic_tcp_listener_server;
 mod multiplex_tcp_listener_server;
 mod pipeline_tcp_listener_server;
 
+pub use self::multiplex_tcp_client::MultiplexTcpClient;
 pub use self::pipeline_tcp_client::PipelineTcpClient;
 
 pub use self::multiplex_tcp_server::MultiplexTcpServer;
---
+++ src/tcp/multiplex_tcp_client.rs
@@ -0,0 +1,62 @@
+use std::{hash::Hash, net::SocketAddr};
+
+use tokio_core::reactor::Handle;
+use tokio_io::codec::{Decoder, Encoder};
+use tokio_service::Service;
+
+use super::{
+    super::{
+        client_error::ClientError, message_with_id::MessageWithId,
+        multiplex_client::MultiplexClient,
+    },
+    tcp_client_transport::TcpClientTransport,
+};
+
+pub struct MultiplexTcpClient<C>
+where
+    C: Decoder + Encoder,
+    <C as Decoder>::Item: MessageWithId,
+    <C as Encoder>::Item:
+        MessageWithId<Id = <<C as Decoder>::Item as MessageWithId>::Id>,
+    <<C as Decoder>::Item as MessageWithId>::Id: Eq + Hash,
+{
+    client: MultiplexClient<TcpClientTransport<C>>,
+}
+
+impl<C> MultiplexTcpClient<C>
+where
+    C: Decoder + Encoder,
+    <C as Decoder>::Item: MessageWithId,
+    <C as Encoder>::Item:
+        MessageWithId<Id = <<C as Decoder>::Item as MessageWithId>::Id>,
+    <<C as Decoder>::Item as MessageWithId>::Id: Eq + Hash,
+{
+    pub fn connect(address: &SocketAddr, codec: C, handle: &Handle) -> Self {
+        let transport = TcpClientTransport::connect(address, codec, handle);
+
+        MultiplexTcpClient {
+            client: MultiplexClient::new(transport),
+        }
+    }
+}
+
+impl<'s, C> Service for &'s MultiplexTcpClient<C>
+where
+    C: Decoder + Encoder,
+    <C as Decoder>::Item: MessageWithId,
+    <C as Encoder>::Item:
+        MessageWithId<Id = <<C as Decoder>::Item as MessageWithId>::Id>,
+    <<C as Decoder>::Item as MessageWithId>::Id: Eq + Hash,
+{
+    type Request = <C as Encoder>::Item;
+    type Response = <C as Decoder>::Item;
+    type Error = ClientError<<C as Decoder>::Error, <C as Encoder>::Error>;
+    type Future =
+        <&'s MultiplexClient<TcpClientTransport<C>> as Service>::Future;
+
+    fn call(&self, request: Self::Request) -> Self::Future {
+        let client = &self.client;
+
+        client.call(request)
+    }
+}
